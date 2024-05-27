41fe75fb174578b1e35a3bb1d1a2b4c35a08ce00
Tomáš Kelemen (vudiq) <vudiq@vudiq.sk>
2024-05-27 16:07:58 +0200

    same_cdi/patch: add missing include + enable core

--- packages/lakka/libretro_cores/package.mk
+++ packages/lakka/libretro_cores/package.mk
@@ -266,7 +266,7 @@ if [ -n "${CUSTOM_LIBRETRO_CORES}" ]; then
 fi
 
 # temporary disabled due to build errors for all targets
-for core in citra pcsx2 same_cdi ; do
+for core in citra pcsx2 ; do
   LIBRETRO_CORES="${LIBRETRO_CORES// ${core} /}"
 done
 
