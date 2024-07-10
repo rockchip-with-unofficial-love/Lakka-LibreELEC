7fa096594c3909b429225b61db7c06b23d1b5b9b
Demetris Ierokipides <ierokipides.dem@gmail.com>
2024-07-10 03:32:46 +0300

    Add proper rpi5 build flags closes https://github.com/libretro/Lakka-LibreELEC/issues/1982

--- packages/lakka/libretro_cores/mupen64plus_next/package.mk
+++ packages/lakka/libretro_cores/mupen64plus_next/package.mk
@@ -55,7 +55,7 @@ pre_make_target() {
       PKG_MAKE_OPTS_TARGET+=" platform=rpi4_64-mesa FORCE_GLES3=1"
       ;;
     RPi5)
-      PKG_MAKE_OPTS_TARGET+=" platform=rpi-mesa FORCE_GLES3=1 CPUFLAGS="
+      PKG_MAKE_OPTS_TARGET+=" platform=rpi5"
       ;;
     Exynos)
       PKG_MAKE_OPTS_TARGET+=" platform=odroid BOARD=ODROID-XU"
